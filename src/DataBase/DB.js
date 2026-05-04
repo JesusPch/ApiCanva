8e174d4fb82bbc422c1be506e23863191a195e2e
JesusPch <jm.pacheco23@info.uas.edu.mx>
2026-05-03 20:51:25 -0700

    actualizacion de nueba base de datos en zorin OS

--- src/DataBase/DB.js
+++ src/DataBase/DB.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 export const sequelize = new Sequelize(
     'proyectodb',
     'postgres',
-    '1287',
+    '101287',
     {
      host:"localhost",
      dialect:"postgres"
